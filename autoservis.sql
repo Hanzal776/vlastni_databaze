c75520c4ed594b9a9dda082897f2472930e05176
pissmaster <148314115+Hanzal776@users.noreply.github.com>
2025-12-05 10:11:55 +0100

    Add 'not null' constraints to database tables

--- autoservis.sql
+++ autoservis.sql
@@ -1,53 +1,53 @@
 Table osoba {
-  osoba_id int [pk]
-  jmeno varchar(30)
-  prijmeni varchar(30)
-  telefon varchar(20) [unique]
-  email varchar(50) [unique]
+  osoba_id int [pk, not null]
+  jmeno varchar(30) [not null]
+  prijmeni varchar(30) [not null]
+  telefon varchar(20) [unique, not null]
+  email varchar(50) [unique, not null]
 }
 
 Table zakaznik {
-  zakaznik_id int [pk, ref: > osoba.osoba_id]
-  datum_registrace date
+  zakaznik_id int [pk, ref: > osoba.osoba_id, not null]
+  datum_registrace date [not null]
 }
 
 Table zamestnanec {
-  zamestnanec_id int [pk, ref: > osoba.osoba_id]
-  pozice enum('Prodejce', 'Mechanik', 'Vedouci', 'Administrativa')
-  mzda decimal(8,2)
+  zamestnanec_id int [pk, ref: > osoba.osoba_id, not null]
+  pozice enum('Prodejce', 'Mechanik', 'Vedouci', 'Administrativa') [not null]
+  mzda decimal(8,2) [not null]
 }
 
 Table auto {
-  auto_id int [pk]
-  znacka varchar(50)
-  model varchar(50)
-  rok_vyroby int
-  obrazek blob
-  najeto_km int
-  cena decimal(10,2)
-  palivo enum('Benzin', 'Nafta', 'Elektro', 'Hybrid')
-  barva varchar(30)
-  prevodovka enum('Manual', 'Automat')
-  dostupne boolean
+  auto_id int [pk, not null]
+  znacka varchar(50) [not null]
+  model varchar(50) [not null]
+  rok_vyroby int [not null]
+  obrazek blob [null]
+  najeto_km int [not null]
+  cena decimal(10,2) [not null]
+  palivo enum('Benzin', 'Nafta', 'Elektro', 'Hybrid') [not null]
+  barva varchar(30) [null]
+  prevodovka enum('Manual', 'Automat') [not null]
+  dostupne boolean [not null]
 }
 
 Table prodej {
-  prodej_id int [pk]
-  zakaznik_id int
-  auto_id int
-  zamestnanec_id int
-  datum_prodeje datetime
-  cena_prodeje decimal(10,2)
+  prodej_id int [pk, not null]
+  zakaznik_id int [not null]
+  auto_id int [not null]
+  zamestnanec_id int [not null]
+  datum_prodeje datetime [not null]
+  cena_prodeje decimal(10,2) [not null]
 }
 
 Table servis {
-  servis_id int [pk]
-  auto_id int
-  zamestnanec_id int
-  popis text
-  cena decimal(8,2)
-  datum_servisu datetime
-  typ_ukonu enum('Garanční', 'Oprava', 'Kontrola', 'Příprava na prodej')
+  servis_id int [pk, not null]
+  auto_id int [not null]
+  zamestnanec_id int [not null]
+  popis text [not null]
+  cena decimal(8,2) [not null]
+  datum_servisu datetime [not null]
+  typ_ukonu enum('Garanční', 'Oprava', 'Kontrola', 'Příprava na prodej') [not null]
 }
 
 Ref: prodej.zakaznik_id > zakaznik.zakaznik_id
